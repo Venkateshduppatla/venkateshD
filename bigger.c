269ec45d4392560aa905fb03a14a363c7a2c0439
Venkatesh.D <venkivenkatesh2699@gmail.com>
2022-04-09 15:17:59 +0530

    mybigger

---
+++ bigger.c
@@ -0,0 +1,21 @@
+// Read two numbers and print the bigger one
+
+#include <stdio.h>
+void main()
+{
+	int number1, number2;
+	printf("Enter two numbers to print the bigger one: ");
+	scanf("%d%d", &number1, &number2);
+	if(number1 == number2)
+	{
+		printf("%d is equal to %d.", number1, number2);
+		return;
+	}
+	if(number1 < number2)
+	{
+		number2 = number1 + number2;
+		number1 = number2 - number1;
+		number2 = number2 - number1;
+	}
+	printf("%d is larger than %d.", number1, number2);
+}
